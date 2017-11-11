497bf91a6c002dc7d30e17ba8962a7a5a47f75b9
Spiro Michaylov <spiro@michaylov.com>
2017-11-11 10:53:37 -0800

    More filtering

--- testdb/TableTest.cpp
+++ testdb/TableTest.cpp
@@ -143,3 +143,52 @@ TEST_F(TableTest, ConjunctiveFilter) {
     EXPECT_EQ(41.9, cost_cursor->get());
     EXPECT_FALSE(fptc.hasMore());
 }
+
+TEST_F(TableTest, NeverTrueFilter) {
+    std::shared_ptr<Table> table;
+    EXPECT_EQ(Status::OK().code(), Tables::createSmallChunkedColumns(table).code());
+    EXPECT_EQ(4, table->num_rows());
+    EXPECT_EQ(2, table->num_columns());
+    ScanTableCursor tc(table);
+
+    std::shared_ptr<Filter> leftFilter = std::make_shared<GreaterThanFilter>("id", 31);
+    std::shared_ptr<Filter> rightFilter = std::make_shared<LessThanFilter>("cost", 22);
+    std::shared_ptr<Filter> andFilter =
+            std::make_shared<AndFilter>("id", leftFilter, rightFilter);
+
+    FilterProjectTableCursor fptc(tc, andFilter);
+
+    uint64_t count = 0;
+    auto id_cursor = std::dynamic_pointer_cast<ColumnCursorWrapper<arrow::Int64Array>>(
+            fptc.getColumn(std::string("id")));
+    auto cost_cursor = std::dynamic_pointer_cast<ColumnCursorWrapper<arrow::DoubleArray>>(
+            fptc.getColumn(std::string("cost")));
+    EXPECT_FALSE(fptc.hasMore());
+}
+
+TEST_F(TableTest, FilterComposition) {
+    std::shared_ptr<Table> table;
+    EXPECT_EQ(Status::OK().code(), Tables::createSmallChunkedColumns(table).code());
+    EXPECT_EQ(4, table->num_rows());
+    EXPECT_EQ(2, table->num_columns());
+    ScanTableCursor tc(table);
+
+    std::shared_ptr<Filter> first_filter = std::make_shared<GreaterThanFilter>("id", 11);
+    FilterProjectTableCursor first_cursor(tc, first_filter);
+
+    std::shared_ptr<Filter> second_filter = std::make_shared<LessThanFilter>("cost", 42);
+    FilterProjectTableCursor second_cursor(first_cursor, second_filter);
+
+    uint64_t count = 0;
+    auto id_cursor = std::dynamic_pointer_cast<ColumnCursorWrapper<arrow::Int64Array>>(
+            second_cursor.getColumn(std::string("id")));
+    auto cost_cursor = std::dynamic_pointer_cast<ColumnCursorWrapper<arrow::DoubleArray>>(
+            second_cursor.getColumn(std::string("cost")));
+    EXPECT_TRUE(second_cursor.hasMore());
+    EXPECT_EQ(12, id_cursor->get());
+    EXPECT_EQ(22.9, cost_cursor->get());
+    EXPECT_TRUE(second_cursor.hasMore());
+    EXPECT_EQ(31, id_cursor->get());
+    EXPECT_EQ(41.9, cost_cursor->get());
+    EXPECT_FALSE(second_cursor.hasMore());
+}
